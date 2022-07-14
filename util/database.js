fe62a636811fabaffd3dbd5f84e020d4141f416d
thiennduyyy <100335382+thiennduyyy@users.noreply.github.com>
2022-07-14 10:19:24 +0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("nodejs", "root", "thienduy", {
-  dialect: "mysql",
-  host: "localhost"
+const sequelize = new Sequelize("ecommerce", "postgres", "pgsecret", {
+  dialect: "postgres",
+  host: "10.0.0.14"
 });
 module.exports = sequelize;
